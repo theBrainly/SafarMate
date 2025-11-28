c74fb8458f19c4a5588dfe53251ab534f2fb9b1b
Akash Sharma <officialmailakashsharma@gmail.com>
2025-11-28 12:00:00 +0530

    Create MainActivity

---
+++ myPP/app/src/main/java/com/example/mypp/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.example.mypp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+import com.example.mypp.navigation.AppNavigation
+import com.example.mypp.ui.theme.MyPPTheme
+
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            MyPPTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    AppNavigation()
+                }
+            }
+        }
+    }
+}
